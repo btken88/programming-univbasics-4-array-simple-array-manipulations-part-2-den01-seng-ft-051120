27f622da1d1e87c7f4f46f52889bfd4a2ec60ae4
Bryce Kennedy <btken88@gmail.com>
2020-04-16 19:54:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
